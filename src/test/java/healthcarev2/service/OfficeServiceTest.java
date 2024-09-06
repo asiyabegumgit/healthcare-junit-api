2ca5635181746fa24cc4c900e9dcfd7aaef40a14
Asiya Begum Sheik Mohamed <asiyabegum.sheikmohamed@gmail.com>
2024-09-06 10:02:25 -0400

    Fix format

--- src/test/java/healthcarev2/service/OfficeServiceTest.java
+++ src/test/java/healthcarev2/service/OfficeServiceTest.java
@@ -11,15 +11,11 @@ import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 
 
 class OfficeServiceTest {
@@ -31,17 +27,18 @@ class OfficeServiceTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         sessionFactory = new Configuration().configure("hibernate-test.cfg.xml").buildSessionFactory();
-        OfficeRepository officeRepository=new OfficeRepositoryImpl(sessionFactory);
-        officeService=new OfficeService(officeRepository);
+        OfficeRepository officeRepository = new OfficeRepositoryImpl(sessionFactory);
+        officeService = new OfficeService(officeRepository);
 
-        DoctorRepository doctorRepository=new DoctorRepositoryImpl(sessionFactory);
-        doctorService=new DoctorService(doctorRepository);
+        DoctorRepository doctorRepository = new DoctorRepositoryImpl(sessionFactory);
+        doctorService = new DoctorService(doctorRepository);
 
     }
+
     @Test
     void testCreateOffice() {
-        Office office=new Office();
-        Doctor doctor=new Doctor();
+        Office office = new Office();
+        Doctor doctor = new Doctor();
         doctor.setFirstName("Katrina");
         doctor.setLastName("Harris");
         doctor.setSpecialty("cardiology");
@@ -51,11 +48,11 @@ class OfficeServiceTest {
         officeService.createOffice(office);
         assertNotNull(office.getOfficeId());
     }
+
     @Test
-    void testUpdateOffice()
-    {
-        Office office=new Office();
-        Doctor doctor=new Doctor();
+    void testUpdateOffice() {
+        Office office = new Office();
+        Doctor doctor = new Doctor();
         doctor.setFirstName("Katrina");
         doctor.setLastName("Harris");
         doctor.setSpecialty("cardiology");
@@ -65,22 +62,22 @@ class OfficeServiceTest {
         officeService.createOffice(office);
         office.setLocation("Texas");
         officeService.updateOffice(office);
-        Office updatedOffice=officeService.getOfficeById(office.getOfficeId());
-        assertEquals("Texas",updatedOffice.getLocation());
+        Office updatedOffice = officeService.getOfficeById(office.getOfficeId());
+        assertEquals("Texas", updatedOffice.getLocation());
 
     }
-    @Test
-    void testDeleteOffice()
-    {
 
-        Doctor doctor=new Doctor();
+    @Test
+    void testDeleteOffice() {
+
+        Doctor doctor = new Doctor();
         doctor.setFirstName("Katrina");
         doctor.setLastName("Harris");
         doctor.setSpecialty("cardiology");
         doctor.setEmail("katrina.harris@gmail.com");
         doctorService.createDoctor(doctor);
 
-        Office office=new Office();
+        Office office = new Office();
         office.setDoctor(doctor);
         office.setLocation("Charlotte");
         office.setPhone("1234567896");
@@ -90,35 +87,37 @@ class OfficeServiceTest {
         assertNotNull(office.getOfficeId());
         int id = doctor.getDoctorId();
         doctorService.deleteDoctor(id);
-        int id1=office.getOfficeId();
+        int id1 = office.getOfficeId();
         officeService.deleteOffice(id1);
 
         assertNull(officeService.getOfficeById(id1));
 
     }
+
     @Test
     void testGetOfficeById() {
-        Office office=officeService.getOfficeById(1);
+        Office office = officeService.getOfficeById(1);
         assertNull(office);
     }
-@ParameterizedTest
-@CsvSource("office_location.csv")
-void testCreateOfficeWithDifferentLocation(String officeName)
-{
-    Office office=new Office();
-    Doctor doctor=new Doctor();
-    doctor.setFirstName("Katrina");
-    doctor.setLastName("Harris");
-    doctor.setSpecialty("cardiology");
-    doctor.setEmail("katrina.harris@gmail.com");
-    doctorService.createDoctor(doctor);
-    office.setDoctor(doctor);
-    office.setLocation(officeName);
-    officeService.createOffice(office);
 
-   assertNotNull(office.getOfficeId());
-   assertEquals(officeName,officeService.getOfficeById(office.getOfficeId()).getLocation());
-}
+    @ParameterizedTest
+    @CsvSource("office_location.csv")
+    void testCreateOfficeWithDifferentLocation(String officeName) {
+        Office office = new Office();
+        Doctor doctor = new Doctor();
+        doctor.setFirstName("Katrina");
+        doctor.setLastName("Harris");
+        doctor.setSpecialty("cardiology");
+        doctor.setEmail("katrina.harris@gmail.com");
+        doctorService.createDoctor(doctor);
+        office.setDoctor(doctor);
+        office.setLocation(officeName);
+        officeService.createOffice(office);
+
+        assertNotNull(office.getOfficeId());
+        assertEquals(officeName, officeService.getOfficeById(office.getOfficeId()).getLocation());
+    }
+
     @AfterEach
     public void tearDown() {
         if (sessionFactory != null) {
